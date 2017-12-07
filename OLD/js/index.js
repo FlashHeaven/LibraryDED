3075428d38171357a22466345bce243e86fc08a2
Bas van Montfort <bas.vanmontfort@hotmail.com>
2017-12-07 21:38:51 +0100

    Tabel gemaakt, naam uitlezen, proberen library van te maken (failed)

--- OLD/js/index.js
+++ OLD/js/index.js
@@ -1,19 +1,29 @@
 var rootRef = firebase.database().ref().child('Gebruikers');
+var CreateUser = function(){
+    
+    var defaultSettings = {
+        naam: 'Empty',
+        rechten: 'Beheerder',
+        status: 'Offline'     
+    }
+    
+    function CreateID(){
+      var text = "";
+      var possible = "0123456789";
 
-function CreateID(){
-  var text = "";
-  var possible = "0123456789";
+      for (var i = 0; i < 1; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-  for (var i = 0; i < 1; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text; 
-};
-
-function AddUser(name){
+      return text; 
+    };
+    
+    function AddUser(){
+    
     rootRef.push({
-           "Naam": name,
+           "Rechten": defaultSettings.rechten,
+           "Naam": defaultSettings.naam,
             "ConnectID": CreateID(),
+            "Status": defaultSettings.status,
             "Groep": {
                 "Naam": "none",
                 "ID": "none"
@@ -22,9 +32,62 @@ function AddUser(name){
     location.href = "main.html";
 };
 
+        var init = function (settings) {
+        mergeObjects(defaultSettings, settings || {});
+       
+        var elements = document.querySelectorAll('input[value][type="button"])'),
+            i;
+        //Loop door elements
+        for (i = 0; i < elements.length; i++) {
+            elements[i].addEventListener("click", function () { 
+            });
+            elements[i].addEventListener("click", addUser);    
+        };
+    };
+
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+function GetLatestInfo(){
+
 rootRef.orderByKey().limitToLast(1).on('child_added',function(snapshot) {
-  console.log('new record', snapshot.val); 
+  console.log('new record', snapshot.val());
+    var naam = snapshot.child("Naam").val();
+    var connectid = snapshot.child("ConnectID").val();
+    var rechten = snapshot.child("Rechten").val();
+    var status = snapshot.child("Status").val();
 });
+};
+
+console.log(GetLatestInfo().naam.val());
+
+function LoadMain(limit){
+
+rootRef.orderByKey().limitToLast(limit).on('child_added',function(snapshot) {
+  console.log('new record', snapshot.val());
+    var naam = snapshot.child("Naam").val();
+    var connectid = snapshot.child("ConnectID").val();
+    var rechten = snapshot.child("Rechten").val();
+    var status = snapshot.child("Status").val();
+    console.log(naam);
+    
+    $("#fireName").append("Hallo " + naam);
+    $("#table_body").append("<tr><td>" + rechten + "</td><td>" + naam + "</td><td>" + connectid + "</td><td>" + status + "</td></tr>");
+    
+});
+};
+
+
 
 
 function getInfo(name){
@@ -41,6 +104,6 @@ function getInfo(name){
         console.log(err);
     }
 
-}
+};
 
 
