79a746b0c3940cf0fbeac684d717dc44a66afe43
Bas van Montfort <bas.vanmontfort@hotmail.com>
2017-12-12 19:31:52 +0100

    Library gefixt en werkt, volgende stap is toevoegen van vriend/familie

--- OLD/js/index.js
+++ OLD/js/index.js
@@ -1,11 +1,19 @@
 var rootRef = firebase.database().ref().child('Gebruikers');
-var CreateUser = function(){
+
+var CreateAdmin = function(Name, Permission){
     
-    var defaultSettings = {
-        naam: 'Empty',
-        rechten: 'Beheerder',
-        status: 'Offline'     
-    }
+    var Settings = {
+        naam: Name,
+        rechten: Permission,
+        status: 'Online',    
+    };
+
+    var init = function() {
+            AddUser();
+    };
+    return {
+        init: init
+    };
     
     function CreateID(){
       var text = "";
@@ -17,93 +25,65 @@ var CreateUser = function(){
       return text; 
     };
     
-    function AddUser(){
-    
-    rootRef.push({
-           "Rechten": defaultSettings.rechten,
-           "Naam": defaultSettings.naam,
-            "ConnectID": CreateID(),
-            "Status": defaultSettings.status,
-            "Groep": {
-                "Naam": "none",
-                "ID": "none"
-            }
-        });
-    location.href = "main.html";
-};
 
-        var init = function (settings) {
-        mergeObjects(defaultSettings, settings || {});
-       
-        var elements = document.querySelectorAll('input[value][type="button"])'),
-            i;
-        //Loop door elements
-        for (i = 0; i < elements.length; i++) {
-            elements[i].addEventListener("click", function () { 
+    function AddUser(){
+
+        rootRef.push({
+               "Rechten": Settings.rechten,
+               "Naam": Settings.naam,
+                "ConnectID": CreateID(),
+                "Status": Settings.status,
+                "Groep": {
+                    "Naam": "none",
+                    "ID": "none"
+                }
             });
-            elements[i].addEventListener("click", addUser);    
-        };
+        location.href = "main.html";
+    }; 
+}
+
+var CreateExtra = function(Name, Permission){
+    
+    var Settings = {
+        naam: Name,
+        rechten: Permission,
+        status: 'Online',    
     };
 
+    var init = function() {
+            AddUser();
+    };
+    return {
+        init: init
+    };
     
+    function CreateID(){
+      var text = "";
+      var possible = "0123456789";
+
+      for (var i = 0; i < 1; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      return text; 
+    };
     
-    
-    
-    
-    
+
+    function AddUser(){
+
+        rootRef.push({
+               "Rechten": Settings.rechten,
+               "Naam": Settings.naam,
+                "ConnectID": CreateID(),
+                "Status": Settings.status,
+                "Groep": {
+                    "Naam": "none",
+                    "ID": "none"
+                }
+            });
+        location.href = "main.html";
+    }; 
 }
 
 
-
-
-
-
-function GetLatestInfo(){
-
-rootRef.orderByKey().limitToLast(1).on('child_added',function(snapshot) {
-  console.log('new record', snapshot.val());
-    var naam = snapshot.child("Naam").val();
-    var connectid = snapshot.child("ConnectID").val();
-    var rechten = snapshot.child("Rechten").val();
-    var status = snapshot.child("Status").val();
-});
-};
-
-console.log(GetLatestInfo().naam.val());
-
-function LoadMain(limit){
-
-rootRef.orderByKey().limitToLast(limit).on('child_added',function(snapshot) {
-  console.log('new record', snapshot.val());
-    var naam = snapshot.child("Naam").val();
-    var connectid = snapshot.child("ConnectID").val();
-    var rechten = snapshot.child("Rechten").val();
-    var status = snapshot.child("Status").val();
-    console.log(naam);
-    
-    $("#fireName").append("Hallo " + naam);
-    $("#table_body").append("<tr><td>" + rechten + "</td><td>" + naam + "</td><td>" + connectid + "</td><td>" + status + "</td></tr>");
-    
-});
-};
-
-
-
-
-function getInfo(name){
-    
-
-    rootRef.on('value', gotData, errData);
-
-    function gotData(data){
-        console.log(data.val());
-     }
-
-    function errData(err){
-        console.log('Error!');
-        console.log(err);
-    }
-
-};
 
 
